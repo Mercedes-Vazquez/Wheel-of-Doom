8410c0a2c7c8692d7783913576852199b1a39fbb
Eni <estefanielunardi@users.noreply.github.com>
2020-10-22 14:31:46 +0200

    still responsive, ball and background

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
 'use strict';
-//"¡after show the name for a few seconds clean the ball¡"?
 
 
 let names = ["Lucas","Laura","Estefanie","Mercedes","Giacomo","Gabri","Giselle","Laura R","Quim","Olga","Moi","Jose","Jackson","Carmen","Alejandra","Lorena","Loredana","Alvaro","Rene","MaxPower","Isma","Jorge","Sergi P","Vanessa"];
 let selected = [];
 
-
-//Click on a buttom and choose one name
 function chooseName(){
     let n = names.length;
     let i = Math.floor(Math.random()*n);
     let selectedName = names[i];
     document.getElementById("contestantName").innerHTML = selectedName;
 
-
-//Take out the choosen name from the array
-    
     names.splice(i, 1);       
     selected.push("<li>" + selectedName + "</li>"); 
     document.getElementById("selectedNames").innerHTML = selected.join('');
     n--;
-
-//When the array name is cero reset the array again
 
     if (n < 1){
         names = selected;
